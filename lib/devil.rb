9946cc21bf41c25d7f4e924bbfd48797e381b8f5
Babar Al-Amin <knock@babar.im>
2016-01-20 22:39:43 +0600

    Improved speed on 4x4 board

--- lib/devil.rb
+++ lib/devil.rb
@@ -3,32 +3,52 @@ require_relative 'player'
 class Devil < Player
   def take_move(board)
     print "Wait, I'm thinking"
-    15.times{print '.'; sleep(0.1)}
+    15.times do
+      print '.'
+      sleep(0.1) if board.format == 3
+    end
 
     if board.first_move?(sign) #Improve initial thinking
       board.mark (board.possible_moves & board.diagonals.flatten).sample, sign
     else
-      board.mark think(board), sign
+      board.mark think(board, 0, {}, (Float::INFINITY * -1), Float::INFINITY, 1, 0), sign
     end
   end
 
   private
-    def think(board, depth=0, scores={})
+    def think(board, depth=0, scores={}, a, b, child_level, start_level)
       return 0 if board.drawn?
       return -1 if board.game_over?
 
+      old_child_level = child_level
+      child_level = 0
+      beta = b
+
       board.possible_moves.each do |move|
+        return 0 if (start_level + 5) < depth
+        child_level += 1 
+
         potential_board = board.dup
         potential_board.mark(move, board.next_move)
-        scores[move] = -1 * think(potential_board, depth + 1, {})
+
+        score = -1 * think(potential_board, depth + 1, {}, -beta, -a, child_level, start_level)
+        if (a < score && score < b && old_child_level != 1)
+          score = -1 * think(potential_board, depth + 1, {}, -b, -a, child_level, start_level)
+        end
+
+        a = [score, a].max
+        b = a + 1
+        
+        scores[move] = a
+        break if a >= b
       end
 
-      best_move, max_simulated_score = scores.max_by { |k, v| v }
 
       if depth == 0
-        best_move
-      elsif depth > 0
-        max_simulated_score
+        scores.max_by { |k, v| v }[0]
+      else
+        a
       end
     end
+
 end
--- readme.md
+++ readme.md
@@ -14,3 +14,5 @@ ruby bin/ttt
 **Optional:**
 
 If you like color output, run `bundle install` from project root and then play!
+
+**Note:** Devil (Computer) might wait a handful of time thinking on bigger board. Don't worry, it's just playing with some 1s and 0s!
